68f0fe91df4adf43a03311e4db718acb28b9736f
Tim Getz <timgetz@gmail.com>
2016-07-30 20:52:21 -0400

    Part 1 of tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+    // [ ] means property binding - C to D
+    // ( ) means event binding - D to C
+
+    title = 'Customer App';
+    name = 'Ward';
+    wardsColor = 'blue';
+
+    changeSuitColor() {
+        this.wardsColor = this.wardsColor === 'blue' ? 'red' : 'blue';
+    }
+}
